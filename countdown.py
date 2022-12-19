3e1e9ec9ae7d10c148164098cee3e752ba04b124
genaffleck <97855765+genaffleck@users.noreply.github.com>
2022-12-20 01:13:05 +0800

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -6,11 +6,11 @@ config.frame_height = 8
 
 class Countdown(Scene):
     def construct(self):
-        k = ValueTracker(10)
+        k = ValueTracker(15)
         num = always_redraw(lambda: DecimalNumber(num_decimal_places=1).set_color(PURE_BLUE).scale(3).set_value(k.get_value()))
         text = Tex("Time's up").scale(3).set_color(PURE_BLUE)
 
         self.play(FadeIn(num))
-        self.play(k.animate.set_value(0), run_time=10, rate_func=linear)
+        self.play(k.animate.set_value(0), run_time=15, rate_func=linear)
         self.play(FadeOut(num))
         self.play(Write(text))
