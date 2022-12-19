fbe54d34d4f63f61952f8af60a578a7959825d51
genaffleck <97855765+genaffleck@users.noreply.github.com>
2022-12-20 01:53:48 +0800

    Create valuetracker.py

---
+++ valuetracker.py
@@ -0,0 +1,10 @@
+from manim import *
+
+class ValueTrackers(Scene):
+    def construct(self):
+        k = ValueTracker(3.5)
+        num = always_redraw(lambda: DecimalNumber().set_value(k.get_value()))
+
+        self.play(FadeIn(num))
+        self.wait()
+        self.play(k.animate.set_value(0), run_time=3.5, rate_func=linear)
